3340a8ad9b8427727398545f3692d990d7b73326
wtao <wtao@users-MacBook-Pro.local>
2018-09-30 17:15:15 -0700

    client app done

--- caerostris/main.js
+++ caerostris/main.js
@@ -6,3 +6,8 @@ const port = 3031;
 app.listen(port, () => console.log(`caerostris is listening on port ${port}!`));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/inbound/:sourceId', (req, res) => {
+    console.log('post received');
+    res.send('Hello World!')
+});
--- clients/echo/App.js
+++ clients/echo/App.js
@@ -6,8 +6,9 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import fetch from 'cross-fetch';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +19,59 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.onPressSend = this.onPressSend.bind(this);
+    this.url = '';
+    this.msg = '';
+  }
+
+  onPressSend() {
+    if (this.url && this.msg) {
+      let url = (this.url + ':3031/inbound/123456').toLowerCase();
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          message: this.message
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(function (response) {
+        let text = response.text();
+        return response.status;
+      }, function (error) {
+        console.log(error.message);
+      })
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to Echo service</Text>
+        <View style={styles.inputRow}>
+          <Text style={styles.instructions}>Server url: </Text>
+          <TextInput style={styles.inputs}
+            placeholder="Enter server url here"
+            onChangeText={text => this.url = text}
+          />
+          <Text style={styles.instructions}>:3031</Text>
+        </View>
+        <View style={styles.inputRow}>
+          <Text style={styles.instructions}>Message: </Text>
+          <TextInput style={styles.inputs}
+            placeholder="Enter message here"
+            onChangeText={text => this.msg = text}
+          />
+          <Text style={styles.instructions} />
+        </View>
+        <TouchableOpacity
+          style={styles.sendButton}
+          onPress={this.onPressSend}
+          underlayColor='#fff'>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -37,13 +85,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    fontSize: 36,
+    textAlign: 'center'
+  },
+  inputRow: {
+    flexDirection: 'row',
+    marginTop: 10
   },
   instructions: {
+    flex: 1,
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    fontSize: 20,
   },
+  inputs: {
+    fontSize: 16,
+    flex: 1,
+  },
+  sendButton: {
+    marginTop: 40,
+    backgroundColor: '#00aeef',
+    width: 100,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontWeight: 'bold'
+  }
 });
